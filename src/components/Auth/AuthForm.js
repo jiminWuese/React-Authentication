092d5f62e08050f152d26d11797c07f017197f55
Jimin Wuese <jiminwuese@gmail.com>
2022-12-03 22:15:20 +0100

    Finished react app authentication

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,28 +1,86 @@
-import { useState } from 'react';
+import { useState, useRef, useContext } from 'react';
+import AuthContext from '../../store/auth-context';
+import { useHistory } from 'react-router-dom';
+import { secretKeys } from './secretKeys';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+
+  const history = useHistory();
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const authCtx = useContext(AuthContext)
+
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    setIsLoading(true);
+    //Add validation if you please.
+    let url;
+    if(isLogin){
+      url = secretKeys.loginKey;
+    }
+    else{
+      url = secretKeys.signupKey;
+    } 
+    fetch(
+      url,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true, 
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      setIsLoading(false);
+      if(res.ok) {
+       return res.json();
+      } else {
+        res.json().then(data => {
+          let errorMessage = 'Authentication failed'; 
+          alert(errorMessage)
+          throw new Error(errorMessage);
+        });
+      }
+    }).then(data => {
+      const expirationTime = new Date(new Date().getTime()+ (+data.expiresIn * 1000))
+      authCtx.login(data.idToken, expirationTime)
+      history.replace('/');
+    }).catch(err => {
+      alert(err.message);
+    })
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' ref={emailInputRef} required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' ref={passwordInputRef} required />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending Request...</p>}
           <button
             type='button'
             className={classes.toggle}
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,42 @@
+import { useRef, useContext } from 'react';
 import classes from './ProfileForm.module.css';
+import AuthContext from '../../store/auth-context';
+import { useHistory } from 'react-router-dom';
+import { ProfileFormKey } from './ProfileFormKey';
 
 const ProfileForm = () => {
+  const history = useHistory();
+  const authCtx = useContext(AuthContext)
+  const changePassRef = useRef();
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = changePassRef.current.value;
+
+    fetch(
+      ProfileFormKey,{
+    method: 'POST',
+    body: JSON.stringify({
+      idToken: authCtx.token,
+      password: enteredNewPassword,
+      returnSecureToken: false
+    }),
+    headers: {
+      'Content-Type': 'Application/json'
+    }
+  }).then(res => {
+    //Assuming this will always succeed.
+    history.replace('/');
+  })
+  }
+
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitFormHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength='7' ref={changePassRef}/>
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
